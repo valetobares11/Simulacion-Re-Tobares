698b5266e8344cc6dda19d43bd898d6493650b1f
nicolasgabrielr <nicolasre58@gmail.com>
2021-04-14 15:09:04 -0300

    Many changes, new Struct, code-style fixes

--- Practica1/Entrega/mm1-estructura.c
+++ Practica1/Entrega/mm1-estructura.c
@@ -3,30 +3,28 @@
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
-//#include "lcgrand.h"  /* Header file for random-number generator. */
-#define TIME_MAX 300.0 /*time limit*/
-#define NUMBER_SERVERS 1 /*number servers*/
-#define Q_LIMIT 100  /* Limit on queue length. */
-#define DIST_LAVADERO 1 
-#define INACTIVO 1.0e+10 /*representa servidos inactivo*/
 #include "Queue.h"
+#define TIME_MAX 300.0 /* time limit */
+#define NUMBER_WASHERS 1 /* NUMBER SERVERS */
+#define Q_LIMIT 100  /* Limit on queue length. */
+#define DIST_LAUNDRY 1 /* Distribución uniforme */
+#define INACTIVE 1.0e+10 /* INF */
 
-float sim_duration;
-float sim_time;
-float time_next_arrivo;
-float time_next_depart[NUMBER_SERVERS];
-//server_depart time_next_depart[NUMBER_SERVERS];
-int  next_event_type;
+typedef struct server_depart {
+    float arrive;
+    float depart;
+} server_depart;
+
+server_depart time_next_depart[NUMBER_WASHERS];
+float current_clock;
+float time_next_arrive;
+int next_event_type;
 int busy_servers;
 int served_clients;
 float time_last_event;
 float queue_total_time;
-int laundry_total_time;
+int commerce_total_time;
 int server_usage;
-float mean_clients_laundry;
-float mean_clients_laundry;
-int clients_in_laundry ;
-float  mean_clients_queue;
 void  initialize(void);
 void  timing(void);
 void  arrive(void);
@@ -42,7 +40,6 @@ void servers_depart();
 void set_seed_random_number(void);
 int search_min_time_server_depart();
 
-
 void set_seed_random_number(void) {
   srand(time(NULL));
 }
@@ -56,7 +53,7 @@ int main()  /* Main function. */
 
     /* Run the simulation while more delays are still needed. */
 
-    while (sim_time <= sim_duration || size() > 0 || busy_servers == NUMBER_SERVERS) {
+    while (current_clock <= TIME_MAX || size() > 0 || busy_servers == NUMBER_WASHERS) {
 
         /* Determine the next event. */
 
@@ -82,7 +79,6 @@ int main()  /* Main function. */
 
     report();
 
-
     return 0;
 }
 
@@ -92,8 +88,8 @@ void initialize(void)  /* Initialization function. */
     /* Initialize the simulation clock. */
     set_seed_random_number();
 
-    sim_duration = TIME_MAX;
-    sim_time = 0.0;
+    current_clock = 0.0;
+
     /* Initialize the state variables. */
     initialization(); //inicializamos la cola
     time_last_event = 0; //timepo del siguiente evento
@@ -101,47 +97,43 @@ void initialize(void)  /* Initialization function. */
 
     // Initialize the statistical counters. num_custs_delayed, total_of_delays, area_num_in_q, area_server_status
     served_clients = 0;
-    laundry_total_time = 0;
+    commerce_total_time = 0;
     server_usage = 0;
     queue_total_time = 0;
-    mean_clients_laundry = 0;
-    mean_clients_queue = 0;
-    clients_in_laundry = 0;
 
     /* Initialize event list.  Since no customers are present, the departure
        (service completion) event is eliminated from consideration. */
 
-    time_next_arrivo = sim_time + gen_next_interarrive(DIST_LAVADERO); //Planificacion del primer arribo
-     for (int i = 0; i < NUMBER_SERVERS; ++i)
+    time_next_arrive = current_clock + gen_next_interarrive(DIST_LAUNDRY); //Planificacion del primer arribo
+    for (int i = 0; i < NUMBER_WASHERS; ++i)
     {
-       time_next_depart[i] = INACTIVO; //Por defecto inician todos los servers inactivos
+      time_next_depart[i].depart = INACTIVE; //Por defecto inician todos los servers INACTIVEs
     }
 }
 
 
 void timing(void)  /* Timing function. */
 {
-    int   i;
-    float min_time_next_event = 1.0e+29;
+   float min_time_next_event = 1.0e+10;
 
-    next_event_type = 0; //Al final debe quedar en 1 en caso de que el proximo evento sea un arribo, 2: si es la salida de un servicio
+   next_event_type = 0; //Al final debe quedar en 1 en caso de que el proximo evento sea un arribo, 2: si es la salida de un servicio
 
-    /* Determine the event type of the next event to occur. */
+   /* Determine the event type of the next event to occur. */
+   min_time_next_event = time_next_arrive;
+   next_event_type = 1;
 
-    min_time_next_event = time_next_arrivo;
-     next_event_type = 1;
-     for (int i = 0; i < NUMBER_SERVERS; ++i)
-      {
-        if (min_time_next_event > time_next_depart[i]) {
-          min_time_next_event = time_next_depart[i];
-          next_event_type = 2;
-        }
-      } 
+   for (int i = 0; i < NUMBER_WASHERS; ++i)
+    {
+      if (min_time_next_event > time_next_depart[i].depart) {
+        min_time_next_event = time_next_depart[i].depart;
+        next_event_type = 2;
+      }
+    } 
 
 
     /* advance the simulation clock. */
 
-    sim_time = min_time_next_event;
+    current_clock = min_time_next_event;
 }
 
 void update_time_avg_stats(void)  /* Update area accumulators for time-average
@@ -151,13 +143,8 @@ void update_time_avg_stats(void)  /* Update area accumulators for time-average
 
     /* Compute time since last event, and update last-event-time marker. */
 
-    time_since_last_event = sim_time - time_last_event;
-    time_last_event       = sim_time;
-
-    /* Update area under number-in-queue function. */
-    mean_clients_queue = mean_clients_queue + ((time_since_last_event)*size());
-    mean_clients_laundry = mean_clients_laundry + ((time_since_last_event)*clients_in_laundry);
-
+    time_since_last_event = current_clock - time_last_event;
+    time_last_event       = current_clock;
 
     /* Update area under server-busy indicator function. */
     server_usage = server_usage +((time_since_last_event)*busy_servers);
@@ -166,23 +153,21 @@ void update_time_avg_stats(void)  /* Update area accumulators for time-average
 
 void arrive(void)  /* Arrival event function. */
 {
-    clients_in_laundry++;
-
     /* Schedule next arrival. */
-    if(sim_time <= sim_duration) {
+    if(current_clock <= TIME_MAX) {
       
-        time_next_arrivo = sim_time + gen_next_interarrive(DIST_LAVADERO);
+        time_next_arrive = current_clock + gen_next_interarrive(DIST_LAUNDRY);
     }
     else {
-      time_next_arrivo = 1.0e+10; 
+      time_next_arrive = 1.0e+10; 
     }
     /* Check to see whether server is busy. */
 
-    if (busy_servers == NUMBER_SERVERS) {
+    if (busy_servers == NUMBER_WASHERS) {
 
         /* Server is busy, so increment number of customers in queue. */
 
-	      add(sim_time);
+	      add(current_clock);
 
         /* Check to see whether an overflow condition exists. */
 
@@ -203,15 +188,16 @@ void arrive(void)  /* Arrival event function. */
 
         /*Llevar el numero de clientes que han entrado*/
 
-        served_clients++;	
         /* Schedule a departure (service completion). */
 
         //arriva el cliente a un server y pasa a estar ocupado
-        int i =0;
-        while (time_next_depart[i] != INACTIVO){
+        int i = 0;
+
+        while (time_next_depart[i].depart != INACTIVE){
           i++;
         }
-        time_next_depart[i] = sim_time + gen_next_depart();
+        time_next_depart[i].arrive = current_clock;
+        time_next_depart[i].depart = current_clock + gen_next_depart();
         busy_servers++;
     }
 }
@@ -219,9 +205,7 @@ void arrive(void)  /* Arrival event function. */
 
 void depart(void)  /* Departure event function. */
 {
-     
-    served_clients++;
-    clients_in_laundry--;
+    served_clients++;  
 
     /* Check to see whether the queue is empty. */
     int min = search_min_time_server_depart();
@@ -229,7 +213,8 @@ void depart(void)  /* Departure event function. */
 
         /* The queue is empty so make the server idle and eliminate the
            departure (service completion) event from consideration. */
-        time_next_depart[min] = INACTIVO;
+        commerce_total_time += time_next_depart[min].depart - time_next_depart[min].arrive;
+        time_next_depart[min].depart = INACTIVE;
         busy_servers--;
     }
 
@@ -243,25 +228,24 @@ void depart(void)  /* Departure event function. */
         /* Compute the delay of the customer who is beginning service and update
            the total delay accumulator. */
         
-
         /* Increment the number of customers delayed, and schedule departure. */
 
-        time_next_depart[min] = sim_time + gen_next_depart();
-        laundry_total_time += (sim_time - time_cte) + (time_next_depart[min]-sim_time);
-        queue_total_time += (sim_time - time_cte);
-
+        time_next_depart[min].arrive = current_clock;
+        time_next_depart[min].depart = current_clock + gen_next_depart();     
+        commerce_total_time += (current_clock - time_cte) + (time_next_depart[min].depart - current_clock);
+        queue_total_time += (current_clock - time_cte);
     }
 }
 int search_min_time_server_depart()
 {
 
-    float time_min= 1.0e+10;
-    int min=0;
+    float time_min = 1.0e+10;
+    int min = 0;
    
-    for (int i = 0; i < NUMBER_SERVERS; ++i)
+    for (int i = 0; i < NUMBER_WASHERS; ++i)
     {
-        if (time_min > time_next_depart[i]) {
-            time_min = time_next_depart[i];
+        if (time_min > time_next_depart[i].depart) {
+            time_min = time_next_depart[i].depart;
             min = i;
         }
     }
@@ -272,42 +256,39 @@ int search_min_time_server_depart()
 
 void report(void)  /* Report generator function. */
 {
+
+    float areaUsoServidor = (server_usage / time_last_event*NUMBER_WASHERS);
+    float areaUsoCola = queue_total_time / time_last_event;
+
     /* Compute and write estimates of desired measures of performance. */
-/* Compute and write estimates of desired measures of performance. */
-    printf("Tiempo promedio de un cliente en la cola.: %f\n",queue_total_time / (served_clients+0.0)); //media de demora en cola //
+    printf("Tiempo promedio de un cliente en la cola: %.1lfm\n",queue_total_time / (served_clients+0.0)); //media de demora en cola
 
-    printf("Tiempo promedio que un cliente pasa en el comercio: %f\n", laundry_total_time / (served_clients+0.0)); //media de demora en el local //
+    printf("Tiempo promedio que un cliente pasa en el comercio: %.1lfm\n", commerce_total_time / (served_clients+0.0));  //media de demora en el local
 
-    printf("Número promedio de clientes en la cola: %f\n",mean_clients_queue / time_last_event ); //media de clientes en cola //
+    printf("Número promedio de clientes en la cola: %.2lf\n", areaUsoCola); //media de clientes en cola
 
-    printf("Número promedio de clientes en el comercio: %f\n",mean_clients_laundry / time_last_event ); //media de clientes en local //.
+    printf("Número promedio de clientes en el comercio: %.2lf\n", areaUsoCola + areaUsoServidor); //media de clientes en local
 
-    printf("Utilización de la máquina lavadora de autos: %f\n",server_usage / (time_last_event*NUMBER_SERVERS)); //.
-
+    printf("Porcentaje de utilización de %d lavadora/s: %%%.2lf\n", NUMBER_WASHERS, server_usage / (time_last_event*NUMBER_WASHERS)); //porcentaje de utilizacion de los n servidores
 
 }
 
 
-/*Generates the next arrivo in base of given probabilities*/
+/*Generates the next arrive in base of given probabilities*/
 float gen_next_interarrive(int dist)
 {
   
      if(dist == 1){
         return gen_interarrive_uniforme(); 
      }
-     /* if(dist == 2){
-        return exponencial();
-     }
-      if(dist == 3){
-        return normal();
-     }*/
+
     return 0;    
 }
 
 
 
 /*Generates the next depart in base of given probabilities*/
-float  gen_next_depart()
+float gen_next_depart()
 {
     double randomNumber = generate_random_number();
     if (randomNumber <= 0.15) {
@@ -340,42 +321,42 @@ double generate_random_number(void) {
 
 float gen_interarrive_uniforme()
 {
-double randomNumber = generate_random_number();
-  if (randomNumber <= 0.05) {
-    return 5.0;
-  }
-  else {
-    if (randomNumber <= 0.1) {
-      return 10.0;
+  double randomNumber = generate_random_number();
+    if (randomNumber <= 0.05) {
+      return 5.0;
     }
     else {
-      if (randomNumber <= 0.2) {
-        return 15.0;
+      if (randomNumber <= 0.1) {
+        return 10.0;
       }
       else {
-        if (randomNumber <= 0.3) {
-          return 20.0;
+        if (randomNumber <= 0.2) {
+          return 15.0;
         }
         else {
-          if (randomNumber <= 0.6) {
-            return 25.0;
+          if (randomNumber <= 0.3) {
+            return 20.0;
           }
           else {
-            if (randomNumber <= 0.8) {
-              return 30.0;
+            if (randomNumber <= 0.6) {
+              return 25.0;
             }
             else {
-              if (randomNumber <= 0.95) {
-                return 35.0;
+              if (randomNumber <= 0.8) {
+                return 30.0;
               }
               else {
-                return 40.0;
+                if (randomNumber <= 0.95) {
+                  return 35.0;
+                }
+                else {
+                  return 40.0;
+                }
               }
             }
           }
         }
       }
-    }
   }
 
 }
